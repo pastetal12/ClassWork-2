b8341d81cd7cca38acee49258f23caeb425371c7
pastetal12 <pastetal12@gmail.com>
2019-12-01 21:50:34 +0200

    Adding project to repo

---
+++ BankManager/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+      int smallCreditValue = 50;
+      int mediumCreditValue = 100;
+      int bigCreditValue = 500;
+      int currentDepositValue = 800;
+
+      String smallCredit = "Small Credit";
+      String bigCredit = "Big Credit";
+      String mediumCredit = "Medium Credit";
+      String currentDeposit = "Current Deposit";
+
+
+      System.out.println(smallCreditValue + "\n");
+      System.out.println(smallCredit + "\n");
+      System.out.println(bigCreditValue + "\n");
+      System.out.println(bigCredit + "\n");
+      System.out.println(mediumCredit + "\n");
+      System.out.println(mediumCreditValue + "\n");
+      System.out.println(currentDeposit + "\n");
+      System.out.println(currentDepositValue + "\n");
+
+
+      String[] creditCollection = {smallCredit, bigCredit, mediumCredit};
+
+
+      System.out.println(creditCollection.length);
+      int lastCreditProductIndex = creditCollection.length - 1;
+      System.out.println(creditCollection[lastCreditProductIndex]);
+
+      System.out.println(creditCollection[0]); }
+
+}
